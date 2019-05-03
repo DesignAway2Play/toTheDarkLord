2fb5b85927de9105624b6de29fe90619c0f81577
Timothy 2 <timothy2@Timothys-MacBook-Air.local>
2019-05-03 14:56:11 -0700

    initializing game js

--- js/darkLord.js
+++ js/darkLord.js
@@ -1,11 +1,12 @@
 // const vars
-
+// rules as an image or dynamic? prob dynamic if I want to change the payouts
+// const bosses
 
 // cached elements
 
 //state vars
 
-let playArea,
+let playArea, pScore, bonus, results, cScore
 
 let winCheck = {
 
@@ -23,22 +24,55 @@ let pScore = {
 
 function initialize() {
   playArea = {
-    col1: 'assets/img/blank.png',
-    col2: 'assets/img/blank.png',
-    col3: 'assets/img/blank.png',
-    col4:  'assets/img/blank.png',
-  };
-  {
-    pScore = {
-    s: 0,
-  };
-  // display rocks at start
-  results = {
-    p: 'r',
-    c: 'r'
-  };
-  winner = null;
+      x1icon_1: {
+        imgURL = 'assets/img/blank.png',
+      },
+      x1icon_2: {
+        imgURL = 'assets/img/blank.png',
+      },
+      x2icon: {
+        imgURL = 'assets/img/blank.png',
+      },
+      x3icon:  {
+        imgURL = 'assets/img/blank.png',
+      },
+      x2icon: {
+        imgURL = 'assets/img/blank.png',
+      },
+      x3icon:  {
+        imgURL = 'assets/img/blank.png',
+      }
+    };
+  pScore = {
+      s: 1000,
+      stam: 3,
+      results: ["", "", "", ""],
+    };
+  bonus:  {
+    d20: [true, false, false, false, false, false, false,
+        false, false, false, false, false, false, false,
+      false, false, false, false, false, false],
+    addTrue: function(){
+        d20.pop();
+        d20.unshift(true);
+      },
+    };
+
+
   render();
+}
+
+function render() {
+  // render scores
+  pScoreEl.textContent = scores.p;
+  cScoreEl.textContent = scores.c;
+  tScoreEl.textContent = scores.t;
+  // render images
+  pResultEl.style.backgroundImage = `url(${rps[results.p].imgUrl})`;
+  cResultEl.style.backgroundImage = `url(${rps[results.c].imgUrl})`;
+  // color border if winner, otherwise "hide" the border
+  pResultEl.parentElement.style.border = winner === 'p' ? '10px solid darkgrey' : '10px solid white';
+  cResultEl.parentElement.style.border = winner === 'c' ? '10px solid darkgrey' : '10px solid white';
 }
 
  function playRoll() {
