7cb5018457718effce601ddfac19c2a6cd33351b
Timothy 2 <timothy2@Timothys-MacBook-Air.local>
2019-05-03 13:14:56 -0700

    committing the first

---
+++ js/darkLord.js
@@ -0,0 +1,58 @@
+// const vars
+
+
+// cached elements
+
+//state vars
+
+let playArea,
+
+let winCheck = {
+
+};
+
+let pScore = {
+  s: 0,
+};
+
+// event listeners
+
+
+
+//functions
+
+function initialize() {
+  playArea = {
+    col1: 'assets/img/blank.png',
+    col2: 'assets/img/blank.png',
+    col3: 'assets/img/blank.png',
+    col4:  'assets/img/blank.png',
+  };
+  {
+    pScore = {
+    s: 0,
+  };
+  // display rocks at start
+  results = {
+    p: 'r',
+    c: 'r'
+  };
+  winner = null;
+  render();
+}
+
+ function playRoll() {
+
+ };
+
+function payOUt() {
+
+}
+
+ function reset() {
+
+ };
+
+ function credit() {
+
+ };
